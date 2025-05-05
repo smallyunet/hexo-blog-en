5681baf5dfb1fb876767bf2f03d234b731d32995
smallyunet <dev.wangyu@gmail.com>
2025-05-05 19:38:02 +0800

    update
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

--- themes/yinwang/source/js/main.js
+++ themes/yinwang/source/js/main.js
@@ -3,7 +3,7 @@ function getUrlRelativePath() {
   var arrUrl = url.split("//");
 
   var start = arrUrl[1].indexOf("/");
-  var relUrl = arrUrl[1].substring(start); 
+  var relUrl = arrUrl[1].substring(start);
 
   if (relUrl.indexOf("?") != -1) {
     relUrl = relUrl.split("?")[0];
@@ -11,23 +11,8 @@ function getUrlRelativePath() {
   return relUrl;
 }
 
-function preloadMicroBlog(year) {
-  if (getUrlRelativePath() == "/") {
-    let url = `/micro-blog/${year}.json`;
-    $.get(url, (res) => {
-      localStorage.setItem(`micro-blog-${year}`, JSON.stringify(res));
-    });
-  }
-}
-
-// 入口
 $(() => {
   document.querySelectorAll("pre code").forEach((block) => {
     hljs.highlightBlock(block);
   });
-
-  preloadMicroBlog(2020);
-  preloadMicroBlog(2021);
-  preloadMicroBlog(2022);
-  preloadMicroBlog(2023);
 });
